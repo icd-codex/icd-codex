d3e2c932c6f335681f95df2c1cf7c7e8a29b4d5f
Jeremy Fisher <jeremy.adams.fisher@gmail.com>
2025-12-31 21:43:58 -0800

    Bump version: 0.5.2 → 0.5.3

--- icdcodex/__init__.py
+++ icdcodex/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Jeremy Fisher"
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 
 from . import hierarchy, icd2vec
 
--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/icd-codex/icd-codex',
-    version='0.5.2',
+    version='0.5.3',
     zip_safe=True,
 )
